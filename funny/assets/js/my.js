41923befe77e1619be08b9e3b86545dcaa293d3c
LinJian <36122549+linxiansheng2688@users.noreply.github.com>
2019-05-23 15:04:14 +0800

    Add files via upload

---
+++ funny/assets/js/my.js
@@ -0,0 +1,14 @@
+
+$(function () {
+    var $personal = $(".J-personal");
+    setInterval(getTime(),1000); //每隔一秒运行一次
+    //取得系统当前时间
+    function getTime(){
+        var myDate = new Date();
+        var date = myDate.toLocaleDateString();
+        var hours = myDate.getHours();
+        var minutes = myDate.getMinutes();
+        var seconds = myDate.getSeconds();
+        $("#showDate").html(date+" "+hours+":"+minutes+":"+seconds); //将值赋给div
+    }
+})
